f47ba02a6e9d7cf6b9862268fe432ea6b6c094e9
Derek O'Brien <derekobrien40@gmail.com>
2023-02-17 14:43:44 -0500

    Remove dummy card divs

--- books.js
+++ books.js
@@ -1,7 +1,6 @@
 /* eslint-disable require-jsdoc */
 
 // Find a way to use the DOM to add .card divs when running addBookToLibrary
-// 
 
 const myLibrary = [];
 
@@ -15,27 +14,70 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary(book) {
-  book.title = prompt('What is the title of the book?');
-  book.author = prompt('Who is the author of the book?');
-  book.pages = prompt('How many pages are in the book?');
-  book.read = prompt('Did you finish the book?');
-  if (book.read === yes) {
-    book.read = true;
-  } else {
-    book.read = false;
-  }
-  myLibrary.push(book);
+addBookToLibrary = function() {
+  title = prompt('What is the title of the book?');
+  author = prompt('Who is the author of the book?');
+  pages = prompt('How many pages are in the book?');
+  read = prompt('Did you finish the book?');
+  //   if (this.read === yes) {
+  //     this.read = true;
+  //   } else {
+  //     this.read = false;
+  //   }
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  createBookCard(newBook);
   // Figure out what needs to go in here so that the input gets added to array
+};
+
+function NewBook() {
+
 }
 
-addBookToLibrary.prototype = Object.create(Book.prototype);
+NewBook.prototype = Object.create(Book.prototype);
 
-addBookToLibrary();
+NewBook();
 
-const ballsBook = new Book('balls', 'Derek', '500', true);
+const myBook = new Book('book', 'Derek', '500', true);
 
-console.log(ballsBook.info());
+console.log(myBook.info());
 console.log(myLibrary);
 
+// User Interface
+const container = document.querySelector('.container');
+const addBook = document.querySelector('#add-book');
 
+addBook.addEventListener('click', addBookToLibrary);
+
+const createBookCard = (newBook) => {
+  const card = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const readBtn = document.createElement('button');
+  const removeBtn = document.createElement('button');
+
+  card.classList.add('card');
+//   readBtn.onclick = toggleRead;
+//   removeBtn.onclick = removeBook;
+
+  title.textContent = `"${newBook.title}"`;
+  author.textContent = newBook.author;
+  pages.textContent = `${newBook.pages} pages`;
+  removeBtn.textContent = 'Remove';
+
+  if (newBook.isRead) {
+    readBtn.textContent = 'Read';
+    readBtn.classList.add('btn-light-green');
+  } else {
+    readBtn.textContent = 'Not read';
+    readBtn.classList.add('btn-light-red');
+  }
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(readBtn);
+  card.appendChild(removeBtn);
+  container.appendChild(card);
+};
