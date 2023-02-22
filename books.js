977be43db786ee68489156d26f51ae07b69b9293
Derek O'Brien <derekobrien40@gmail.com>
2023-02-22 14:57:32 -0500

    Add new functions, clean up code

--- books.js
+++ books.js
@@ -4,75 +4,49 @@
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  };
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
 }
 
-addBookToLibrary = function() {
-  openForm();
-    // title = prompt('What is the title of the book?');
-    // author = prompt('Who is the author of the book?');
-    // pages = prompt('How many pages are in the book?');
-    // read = prompt('Did you finish the book?');
-  const newBook = new Book(title, author, pages, true);
-  myLibrary.push(newBook);
-  createBookCard(newBook);
-  // Figure out what needs to go in here so that the input gets added to array
-};
-
-const myBook = new Book('book', 'Derek', '500', true);
-
-console.log(myBook.info());
-console.log(myLibrary);
-
-// User Interface
+// Global Variables
 const container = document.querySelector('.container');
 const addBook = document.querySelector('#add-book');
-const submitForm = document.querySelector('#submit-form');
 const formDiv = document.querySelector('.form-div');
 const body = document.querySelector('.body');
 const backgroundDiv = document.createElement('div');
 backgroundDiv.setAttribute('id', 'background');
 
-// Form Data
-const bookForm = document.querySelector('#book-form');
-// bookForm.addEventListener('submit', callbackFunction);
+// Input Variables
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const isReadInput = document.querySelector('#read');
 
-// function callbackFunction(event) {
-//   event.preventDefault();
-//   const myFormData = new FormData(event.target);
-//   const formDataObj = Book.fromEntries(myFormData.entries());
-//   console.log(formDataObj);
-// }
-
-const formData = new FormData(bookForm);
-console.log(formData);
-
-addBook.addEventListener('click', addBookToLibrary);
-submitForm.addEventListener('click', closeForm);
-
-function openForm() {
-  document.querySelector('.form-div').style.display = 'block';
-//   backgroundDiv.appendChild(body);
-//   backgroundDiv.style.backgroundColor = 'black|transparent';
+// Functions
+function createBookObject(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  return newBook;
 }
-function closeForm() {
-  document.querySelector('.form-div').style.display = 'none';
-}
-window.onclick = function(event) {
-  const modal = document.querySelector('.form-div');
-  if (event.target == modal) {
-    closeForm();
-  }
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  return newBook;
 };
 
-const createBookCard = (newBook) => {
+function isReadChecked() {
+  const isRead = isReadInput.checked;
+  return isRead;
+}
+
+console.log(isReadInput.checked);
+console.log(isReadChecked());
+
+function createBookCard(newBook) {
   const card = document.createElement('div');
   const title = document.createElement('p');
   const author = document.createElement('p');
@@ -89,7 +63,7 @@ const createBookCard = (newBook) => {
   pages.textContent = `${newBook.pages} pages`;
   removeBtn.textContent = 'Remove';
 
-  if (newBook.read) {
+  if (newBook.isRead) {
     readBtn.textContent = 'Read';
     readBtn.classList.add('btn-light-green');
   } else {
@@ -104,3 +78,41 @@ const createBookCard = (newBook) => {
   card.appendChild(removeBtn);
   container.appendChild(card);
 };
+
+// User Interface
+
+
+// Form Data
+const bookForm = document.querySelector('#book-form');
+const submitForm = document.querySelector('#submit-form');
+
+// bookForm.addEventListener('submit', function(e) {
+//   e.preventDefault();
+//   getData(e.target);
+// });
+
+addBook.addEventListener('click', openForm);
+submitForm.addEventListener('click', () => {
+  closeForm();
+  addBookToLibrary(
+      createBookObject(titleInput.value, authorInput.value,
+          pagesInput.value, isReadInput.checked));
+  createBookCard(newBook);
+});
+
+function openForm() {
+  document.querySelector('.form-div').style.display = 'block';
+//   backgroundDiv.appendChild(body);
+//   backgroundDiv.style.backgroundColor = 'black|transparent';
+}
+function closeForm() {
+  document.querySelector('.form-div').style.display = 'none';
+}
+window.onclick = function(event) {
+  const modal = document.querySelector('.form-div');
+  if (event.target == modal) {
+    closeForm();
+  }
+};
+
+// console.log(newBook);
